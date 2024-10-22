a889a8f6ab71eccb25993c48f8d97003552fa6a8
Harish <hchaura1@asu.edu>
2024-10-21 17:15:09 -0700

    practised basics

--- python.py
+++ python.py
@@ -1,1 +1,14 @@
-print("Hello World")
+# print("Hello World")
+
+
+# Create a list of integers from 1 to 10. Use list slicing to print only the first 5 elements.
+# new_list = [1,2,3,4,5,6,7,8,9,10]
+# print(new_list[0:5])
+
+# Write a Python function that takes a list and returns a list of its elements squared.
+# lst = [0,1,2,45,3,2,4,5,4]
+# squared = (n**2 for n in lst)
+# print(list(squared))
+# print(squared)
+# for n in squared:
+#     print(n)
